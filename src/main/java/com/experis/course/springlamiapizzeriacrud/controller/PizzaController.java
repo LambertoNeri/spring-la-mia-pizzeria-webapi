ccb92b4dc67e51d16ba7fc5be712de7216384ff9
LambertoNeri <neri.lamberto91@gmail.com>
2023-11-10 16:01:14 +0100

    exercise completation

--- src/main/java/com/experis/course/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/com/experis/course/springlamiapizzeriacrud/controller/PizzaController.java
@@ -3,12 +3,18 @@ package com.experis.course.springlamiapizzeriacrud.controller;
 import com.experis.course.springlamiapizzeriacrud.model.Pizza;
 import com.experis.course.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
+
 
 @Controller
 @RequestMapping("/pizzas")
@@ -21,11 +27,32 @@ public class PizzaController {
   // METODO CHE MOSTRA LA LISTA DI TUTTE LE PIZZE
 
   @GetMapping
-  public String index(Model model) {
-    // pizzaRepository recupera da database la lista di tutte le pizze
-    List<Pizza> pizzaList = pizzaRepository.findAll();
-    // passo al template la lista di tutte le pizze
+  public String index(@RequestParam Optional<String> search,
+                      Model model) {
+    List<Pizza> pizzaList;
+
+    if (search.isPresent()) {
+      pizzaList = pizzaRepository.findByNameContainingIgnoreCase(search.get());
+    } else {
+      // pizzaRepository recupera da database la lista di tutte le pizze
+      pizzaList = pizzaRepository.findAll();
+      // passo al template la lista di tutte le pizze
+    }
     model.addAttribute("pizzaList", pizzaList);
     return "pizzas/list";
+  }
+
+  @GetMapping("/show/{id}")
+  public String show(@PathVariable Integer id, Model model) {
+    Optional<Pizza> result = pizzaRepository.findById(id);
+    // verifico se il risultato è presente
+    if (result.isPresent()) {
+      // passo al template l'oggetto Pizza
+      model.addAttribute("pizza", result.get());
+      return "pizzas/show";
+    } else {
+      // se non ho trovato il libro sollevo un'eccezione
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "pizza with id " + id + " not found");
+    }
   }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 INSERT INTO db_pizzeria.pizzas (description, name, pic, price) VALUES('mozzarella, pomodoro', 'Margherita', 'https://primochef.it/wp-content/uploads/2019/08/SH_pizza_fatta_in_casa-640x350.jpg.webp', 5.70);
-INSERT INTO db_pizzeria.pizzas (description, name, pic, price) VALUES('prosciutto cruto, olive nere, carciofini sotto olio, funghi champignon', 'Quattro Stagioni', 'https://www.petitchef.it/imgupl/recipe/pizza-4-stagioni--449891p695427.jpg', 7.50);
-INSERT INTO db_pizzeria.pizzas (description, name, pic, price) VALUES('fiori di zucca, alici, mozzarella', 'Fiori e Alici', 'https://www.ricettedalmondo.it/images/foto-ricette/p/31389-pizza-con-fiori-di-zucca-e-alici.jpg', 6.80);
+INSERT INTO db_pizzeria.pizzas (description, name, pic, price) VALUES('prosciutto cruto, olive nere, carciofini sotto olio, funghi champignon', 'Quattro Stagioni', 'https://www.petitchef.it/imgupl/recipe/pizza-4-stagioni--449891p695427.jpg',7.5);
+INSERT INTO db_pizzeria.pizzas (description, name, pic, price) VALUES('fiori di zucca, alici, mozzarella', 'Fiori e Alici', 'https://www.ricettedalmondo.it/images/foto-ricette/p/31389-pizza-con-fiori-di-zucca-e-alici.jpg', 8.00);
